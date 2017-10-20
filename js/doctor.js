09d31fc77068bb47a59dd2edb4c32acc83a85ebb
erikzaksf <erik-zak-sf@gmail.com>
2017-10-20 16:51:23 -0700

    work on routing data dispite inability to properly build gulp

--- js/doctor.js
+++ js/doctor.js
@@ -8,16 +8,27 @@ $(document).ready(function() {
     let request = new XMLHttpRequest();
     let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${userSearch}&skip=2&limit=10&user_key=` + apiKey;
 
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        let response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    getElements = function(response) {
+      $('.name').text(`Name:${data.profile.first_name}${data.profile.last_name}`);
+      $('.address').text(`Address:${data.practice.visit_address.*}`);
+      $('.phone').text(`Phone:${data.practice.phones[0]}`)
+    }
 
 
 
 
 
-
-  })
-
-
-})
+  });
 
 
-var url =
+});
