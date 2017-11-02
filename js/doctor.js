1f428f2da3366d8aa20df99eb5a59a207899ee23
erikzaksf <erik-zak-sf@gmail.com>
2017-11-01 21:08:09 -0700

    add proper promise to back end, now encountering a then is not defined error

--- js/doctor.js
+++ js/doctor.js
@@ -1,5 +1,6 @@
 var apiKey = require('./../.env').apiKey;
 
+
 export class DoctorSearch {
   constructor(initialize) {
   initialize = parseInt(initialize);
@@ -11,28 +12,28 @@ export class DoctorSearch {
     let promise = new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${userSearch}&skip=2&limit=10&user_key=` + apiKey;
-
-      request.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-          let response = JSON.parse(this.responseText);
-          getElements(response);
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
         }
+        return Promise.all(promises);
       }
-
       request.open("GET", url, true);
       request.send();
 
-      getElements = function(response) {
+      promise.then(function(response) {
+        body = JSON.parse(response);
         $('.name').text(`Name:${data.profile.first_name}${data.profile.last_name}`);
         $('.address').text(`Address:${data.practice.visit_address}`);
         $('.phone').text(`Phone:${data.practice.phones[0]}`)
         $('patients').text(`Accepting new patients: ${doctor.practices[0].accepts_new_patient}`)
-      }
-
-
+      },  function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      });
+      return Promise.all(promises);
     });
-
-
 
   }
 }
